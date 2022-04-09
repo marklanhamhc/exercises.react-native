3e4f0ea9685b989adb572bfa2f2e5a464ca95a86
Mark Lanham <m.lanham@aitch-c.com>
2022-04-09 22:55:11 +0100

    tweaked screen wrapper styling

--- src/components/ScreenWrapper.tsx
+++ src/components/ScreenWrapper.tsx
@@ -1,5 +1,7 @@
+import { BaseSize } from '@config';
 import styled from 'styled-components/native';
 
 export default styled.View`
-  padding: 16px;
+  padding-left: ${BaseSize * 2};
+  padding-right: ${BaseSize * 2};
 `;
