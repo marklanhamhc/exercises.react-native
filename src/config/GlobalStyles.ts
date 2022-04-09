7c3455eeb93002d9b9e9923d20c3f85f9c764c3b
Mark Lanham <m.lanham@aitch-c.com>
2022-04-09 22:55:39 +0100

    Added a global styles config

---
+++ src/config/GlobalStyles.ts
@@ -0,0 +1,8 @@
+import { Colors } from './Colors';
+
+export const BaseSize = 8;
+export const GlobalBorderRadius = 6;
+export const GlobalTouchableHighlight = {
+  underlayColor: Colors.generic.greyGallery,
+  activeOpacity: 0.9
+};
