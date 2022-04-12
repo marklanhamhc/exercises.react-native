94610cfe8dac55d580d9af67a1d9e4bc4e9392e9
Mark Lanham <m.lanham@aitch-c.com>
2022-04-12 10:52:12 +0100

    Added custom colours

---
+++ src/config/Colors.ts
@@ -0,0 +1,18 @@
+// Basic colours
+const BLACK = '#000000';
+const WHITE = '#ffffff';
+
+// Core colour scheme
+const BLACK_HEAVY_METAL = '#343534';
+const BLUE_HAVELOCK = '#4DA1DC';
+const GRAY_GALLERY = '#ececec';
+
+export const Colors = {
+  generic: {
+    black: BLACK,
+    white: WHITE,
+    blackHeavyMetal: BLACK_HEAVY_METAL,
+    blueHavelock: BLUE_HAVELOCK,
+    greyGallery: GRAY_GALLERY
+  }
+};
