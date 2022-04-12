1e6aae9673955c9cfc9bc3ac3da9c2c947a02d73
Mark Lanham <m.lanham@aitch-c.com>
2022-04-12 10:52:01 +0100

    Screen Wrapper moved

--- src/components/ScreenWrapper/ScreenWrapper.tsx
+++ src/components/ScreenWrapper/ScreenWrapper.tsx
@@ -2,6 +2,8 @@ import { BaseSize } from '@config';
 import styled from 'styled-components/native';
 
 export default styled.View`
-  padding-left: ${BaseSize * 2};
+  padding-top: ${BaseSize * 2};
   padding-right: ${BaseSize * 2};
+  padding-bottom: ${BaseSize * 2};
+  padding-left: ${BaseSize * 2};
 `;
