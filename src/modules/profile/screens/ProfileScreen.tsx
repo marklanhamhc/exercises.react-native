d5d1f6a6bfcdd0b0b16f9edca71286858e014258
Mark Lanham <m.lanham@aitch-c.com>
2022-04-12 10:53:14 +0100

    Updated Screen Wrapper import for Profile and Shop screens

--- src/modules/profile/screens/ProfileScreen.tsx
+++ src/modules/profile/screens/ProfileScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
-import ScreenWrapper from '../../../components/ScreenWrapper';
+import ScreenWrapper from '../../../components/ScreenWrapper/ScreenWrapper';
 
 export default () => {
   return (
-    <ScreenWrapper>
-      <Text>Ignore this screen</Text>
+    <ScreenWrapper
+      style={[{ justifyContent: 'center', alignContent: 'center', flex: 1 }]}
+    >
+      <Text style={[{ textAlign: 'center' }]}>Ignore this screen</Text>
     </ScreenWrapper>
   );
 };
